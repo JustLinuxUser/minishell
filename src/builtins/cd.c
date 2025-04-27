44ee805b90bcd2ae55447379c957f35e54e20761
armgonza <riuzakiriveruzumaki@hotmail.com>
2025-04-27 19:53:41 +0200

    bug fixed when cd no args

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -17,67 +17,100 @@ static void update_pwd_vars(t_state *state)
 										.value = ft_strdup(state->cwd.buff)});
 }
 
-int	builtin_cd(t_state *state, t_vec_str argv)
+static void	cd_check_args(t_state *state, t_vec_str argv, int *ret)
 {
-	char	*cwd;
-	char	*home;
-	int		e;
-	char	*oldpwd;
-
-	// t_env	cenv;
 	if (argv.len >= 3)
 	{
-		ft_eprintf("%s: %s: too many arguments\n", state->context,
-			argv.buff[0]);
-		return (1);
+		ft_eprintf("%s: %s: too many arguments\n",
+			state->context, argv.buff[0]);
+		*ret = 1;
 	}
-	home = env_expand(state, "HOME");
+}
+
+static void	cd_do_chdir(t_state *state, t_vec_str argv,
+				char *home, char *oldpwd, int *e, int *ret)
+{
 	if (argv.len == 1)
 	{
 		if (home == NULL)
 		{
 			ft_eprintf("%s: cd: HOME not set\n", state->context);
-			return (1);
+			*ret = 1;
+			return ;
 		}
-		e = chdir(home);
+		*e = chdir(home);
 	}
-	oldpwd = env_expand(state, "OLDPWD");
-	if (!ft_strcmp("-", argv.buff[1]))
+	if (argv.len >= 2 && !ft_strcmp("-", argv.buff[1]))
 	{
 		if (oldpwd == NULL)
-		 	{
+		{
 			ft_eprintf("%s: cd: OLDPWD not set\n", state->context);
-			return(1);
-			}
-		ft_printf("%s\n",oldpwd);
-		e = chdir(oldpwd);
+			*ret = 1;
+			return ;
+		}
+		ft_printf("%s\n", oldpwd);
+		*e = chdir(oldpwd);
 	}
-	else if (argv.len == 2) 
+	else if (argv.len == 2)
 	{
-		e = chdir(argv.buff[1]);
+		*e = chdir(argv.buff[1]);
 	}
-	if (e == -1)
-	{
-		ft_eprintf("%s: %s: %s: %s\n", state->context, argv.buff[0],
-			argv.buff[1], strerror(errno));
-			return(1);
-	}
-	cwd = getcwd(NULL, 0);
+}
+
+static void	cd_refresh_cwd(t_state *state, t_vec_str argv, char *cwd)
+{
 	if (cwd)
 	{
 		state->cwd.len = 0;
 		dyn_str_pushstr(&state->cwd, cwd);
 	}
 	else
-	{		
+	{
 		ft_eprintf("cd: error retrieving current directory: getcwd:"
-				" cannot access parent directories: No such file or directory \n");
+			" cannot access parent directories: No such file or directory \n");
 		if (!dyn_str_ends_with_str(&state->cwd, "/") && state->cwd.buff)
 			dyn_str_pushstr(&state->cwd, "/");
-		dyn_str_pushstr(&state->cwd, argv.buff[1]);
+		if (argv.len == 2)
+			dyn_str_pushstr(&state->cwd, argv.buff[1]);
 	}
+}
+
+int	builtin_cd(t_state *state, t_vec_str argv)
+{
+	char	*cwd;
+	char	*home;
+	int		e = 0;
+	char	*oldpwd;
+	int		ret = 0;
+
+	cd_check_args(state, argv, &ret);
+	if (ret)
+		return (1);
+
+	home = env_expand(state, "HOME");
+	oldpwd = env_expand(state, "OLDPWD");
+	cd_do_chdir(state, argv, home, oldpwd, &e, &ret);
+	if (ret)
+		return (1);
+
+	if (e == -1)
+	{
+		ft_eprintf("%s: %s: %s: %s\n", state->context, argv.buff[0],
+			argv.len >= 2 ? argv.buff[1] : "", strerror(errno));
+		return (1);
+	}
+
+	cwd = getcwd(NULL, 0);
+	cd_refresh_cwd(state, argv, cwd);
 	free(cwd);
-	update_pwd_vars (state);
+	update_pwd_vars(state);
 	return (0);
 }
+
+//manejar el entorno de pwd y oldpwd
+
+// cd - // cd oldpwd fin
+
+//norminettear todo
+
 //exit no imprime ext si no estoy en readline
