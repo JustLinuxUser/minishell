0d48317e9b529cdaf80679d59f17f1ed5baaff98
Andrii Dokhniak <adokhniak@gmail.com>
2025-06-08 02:23:29 +0200

    fix qsort

--- src/glob/qsort.c
+++ src/glob/qsort.c
@@ -27,23 +27,20 @@ int	partition(t_vec_str *vec, int low, int high)
 	int		i;
 	int		p_idx;
 	int		j;
-	char	*p;
 
 	p_idx = (high - low) / 2 + low;
 	swap_sort_point(&vec->buff[low], &vec->buff[p_idx]);
-	p = vec->buff[low];
-	i = low;
+	i = low + 1;
 	j = high;
 	while (i < j)
 	{
-		while (ft_strcmp(vec->buff[i], p) < 0 && i <= high - 1)
+		while (ft_strcmp(vec->buff[i], vec->buff[low]) < 0 && i <= high - 1)
 			i++;
-		while (ft_strcmp(vec->buff[j], p) >= 0 && j >= low + 1)
+		while (ft_strcmp(vec->buff[j], vec->buff[low]) >= 0 && j >= low + 1)
 			j--;
 		if (i < j)
 			swap_sort_point(&vec->buff[i], &vec->buff[j]);
 	}
-	swap_sort_point(&vec->buff[low], &vec->buff[j]);
 	return (j);
 }
 
@@ -62,7 +59,5 @@ void	quicksort_inner(t_vec_str *vec, int low, int high)
 void	ft_quicksort(t_vec_str *vec)
 {
 	if (vec->len > 1)
-	{
 		quicksort_inner(vec, 0, vec->len - 1);
-	}
 }
